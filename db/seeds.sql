2d5579d95436f53bea1b5af900255a0a71cf0dee
hpauzer <heatherpauzer@Heathers-MBP-2.attlocal.net>
2017-11-03 17:06:24 -0500

    sql adds

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Quarterpounder with Cheese", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Turkey Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Double Cheeseburger with Bacon", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Veggie Burger No Bun", true);
